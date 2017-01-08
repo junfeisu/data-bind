ed6c40e0df0de76d5136c111a289fe5576b634b7
sjf <1982764150@qq.com>
2017-01-08 13:57:55 +0800

    12

--- build/js/index.js
+++ build/js/index.js
@@ -1,13 +1,12 @@
-'use strict';
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 (function (global, factory) {
-  (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.sjfDataBind = factory();
-})(typeof window != 'undefined' ? window : undefined, function () {
-  'use strict';
-
-  var priority = {
+  typeof exports === 'object' && typeof module !== 'undefined' ? 
+    module.exports = factory() :
+    typeof define === 'function' && define.amd ? 
+      define(factory) :
+      (global.sjfDataBind = factory())
+})(typeof window != 'undefined' ? window : this, function () {
+  'use strict'
+  const priority = {
     'sjf-for': 1000,
     'sjf-click': 100,
     'sjf-mouseover': 100,
@@ -20,47 +19,49 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     'sjf-change': 100,
     'sjf-model': 10,
     'sjf-text': 1
-  };
-
-  // judge the type of obj
-  var judgeType = function judgeType(obj) {
-    return Object.prototype.toString.call(obj);
-  };
-
-  // compile the sjf
-  var compile = function compile() {
-    var self = this;
-    var html = self._el.innerHTML;
-    var matchExpress = /sjf-.+=\".+\"|\{\{.+\}\}/g;
-    var results = html.match(matchExpress);
-    if (results.length !== 0) {
-      results.forEach(function (value) {
-        if (value.indexOf('=') < 0) {
-          var slices = value.match(/\w+/g);
-          self._watchers.push({ ref: self._el, name: 'text', expression: slices[0], filters: slices[1] });
-        } else {
-          var _slices = value.split('=');
-          self._watchers.push({ ref: self._el, name: _slices[0], expression: _slices[1], filters: _slices[1].split("| ")[1] });
-        }
-      });
-    }
-  };
-
-  var link = function link() {};
-
-  function Sjf(param) {
-    if (!param.hasOwnProperty('el') || !param.hasOwnProperty('data')) {
-      console.error('sjf[error]: There is need `data` and `el` attribute');
-      return;
-    }
-    this._el = document.querySelector(param.el);
-    this._data = param.data;
-    this._watchers = [];
-    for (var method in param.methods) {
-      this['_' + method] = param.methods[method];
-    }
-    compile.call(this);
   }
 
-  return Sjf;
-});
+  // judge the type of obj
+  const judgeType = function (obj) {
+    return Object.prototype.toString.call(obj)
+  }
+
+  // compile the sjf
+  let compile = function () {
+    let self = this
+    let html = self._el.innerHTML
+    let matchExpress = /sjf-.+=\".+\"|\{\{.+\}\}/g
+    let results = html.match(matchExpress)
+    if (results.length !== 0) {
+      results.forEach(value => {
+        if (value.indexOf('=') < 0) {
+          let slices = value.match(/\w+/g)
+          self._watchers.push({ref: self._el, name: 'text', expression: slices[0], filters: slices[1]})
+        } else {
+          let slices = value.split('=')
+          self._watchers.push({ref: self._el, name: slices[0], expression: slices[1], filters: slices[1].split("| ")[1]})
+        }
+      })
+    }
+  }
+
+
+  let link = function () {
+  }
+
+  function Sjf (param) {
+    if (!param.hasOwnProperty('el') || !param.hasOwnProperty('data')) {
+      console.error('sjf[error]: There is need `data` and `el` attribute')
+      return
+    }
+    this._el = document.querySelector(param.el)
+    this._data = param.data
+    this._watchers = []
+    for (var method in param.methods) {
+      this['_' + method] = param.methods[method]
+    }
+    compile.call(this)
+  }
+
+  return Sjf
+})
