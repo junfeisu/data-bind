c8680e5ec6a464cae1e6a68c074852ad7afce51d
sjffly <1982764150@qq.com>
2017-06-28 17:22:52 +0800

    更新Mac系统上dev包不能使用的bug

--- build/js/index.js
+++ build/js/index.js
@@ -1,127 +1,119 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? 
-    module.exports = factory() :
-    typeof define === 'function' && define.amd ? 
-      define(factory) :
-      (global.sjfDataBind = factory())
-})(typeof window != 'undefined' ? window : this, function () {
-  'use strict'
-  const priority = {
+  (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.sjfDataBind = factory();
+})(typeof window != 'undefined' ? window : undefined, function () {
+  'use strict';
+
+  var priority = {
     'sjf-if': 2000,
     'sjf-show': 2000,
     'sjf-for': 1000,
     'sjf-model': 10,
     'sjf-text': 1
-  }
+  };
 
-  const sjfEvents = [
-    'sjf-click', 
-    'sjf-mouseover', 
-    'sjf-mouseout', 
-    'sjf-mousemove', 
-    'sjf-mouseenter',
-    'sjf-mouseleave',
-    'sjf-mousedown',
-    'sjf-mouseup'
-  ]
+  var sjfEvents = ['sjf-click', 'sjf-mouseover', 'sjf-mouseout', 'sjf-mousemove', 'sjf-mouseenter', 'sjf-mouseleave', 'sjf-mousedown', 'sjf-mouseup'];
 
   // judge the type of obj
-  const judgeType = function (obj) {
-    return Object.prototype.toString.call(obj)
-  }
+  var judgeType = function judgeType(obj) {
+    return Object.prototype.toString.call(obj);
+  };
 
   // remove the prefix of sjf-
-  const removePrefix = function (str) {
-    return str = str.replace(/sjf-/, '')
-  }
+  var removePrefix = function removePrefix(str) {
+    return str = str.replace(/sjf-/, '');
+  };
 
   // remove the brackets ()
-  const removeBrackets = function (str) {
-    str = str.replace(/\"/g, '')
-    return str = str.replace(/\(\)/, '')
-  }
+  var removeBrackets = function removeBrackets(str) {
+    str = str.replace(/\"/g, '');
+    return str = str.replace(/\(\)/, '');
+  };
 
   // traverse the DOM
-  function circleElement (parent, isFirst) {
-    let child = parent.children
+  function circleElement(parent, isFirst) {
+    var child = parent.children;
     if (isFirst && !child.length) {
-      link.call(this)
-      return
+      link.call(this);
+      return;
     }
-    for (let i = child.length - 1; i >= 0; i--) {
-      let node = child[i]
+    for (var i = child.length - 1; i >= 0; i--) {
+      var node = child[i];
       if (!!node.children.length) {
-        circleElement.call(this, node, false)
+        circleElement.call(this, node, false);
       } else {
-        compileNode.call(this, node)
+        compileNode.call(this, node);
       }
     }
     if (this._el.lastElementChild === child[child.length - 1]) {
-      link.call(this)
+      link.call(this);
     }
   }
 
-  let compileNode = function (node) {
-    let matchExpress = /sjf-.+=\".+\"|\{\{.+\}\}/
+  var compileNode = function compileNode(node) {
+    var _this = this;
+
+    var matchExpress = /sjf-.+=\".+\"|\{\{.+\}\}/;
     if (matchExpress.test(node.outerHTML)) {
-      let directives = matchExpress.exec(node.outerHTML)
-      directives.forEach((value) => {
-        let slices = value.split('=')
+      var directives = matchExpress.exec(node.outerHTML);
+      directives.forEach(function (value) {
+        var slices = value.split('=');
         if (sjfEvents.indexOf(slices[0]) >= 0) {
-          let eventType = removePrefix(slices[0])
-          let eventFunc = this['_' + removeBrackets[slices[1]]]
-          node.addEventListener(eventType, eventFunc, false)
+          var eventType = removePrefix(slices[0]);
+          var eventFunc = _this['_' + removeBrackets[slices[1]]];
+          node.addEventListener(eventType, eventFunc, false);
         }
-      })
+      });
       node.outerHTML = node.outerHTML.replace(matchExpress, "");
-      this._uncompileNodes.push(node)
+      this._uncompileNodes.push(node);
     }
-  }
+  };
 
   // compile the sjf
-  let compile = function () {
-    circleElement.call(this, this._el, true)
-  }
+  var compile = function compile() {
+    circleElement.call(this, this._el, true);
+  };
 
-  const linkRender = {
-    'sjf-if': function (value) {
-      this.style.display = (!!value ? 'block!important' : 'none!important')
+  var linkRender = {
+    'sjf-if': function sjfIf(value) {
+      this.style.display = !!value ? 'block!important' : 'none!important';
     },
-    'sjf-show': function (value) {
-      this.style.display = (!!value ? 'block!important' : 'none!important')
+    'sjf-show': function sjfShow(value) {
+      this.style.display = !!value ? 'block!important' : 'none!important';
     },
-    'sjf-for': function () {
-
-    },
-    'sjf-text': function (value) {
-      this.innerText = value
+    'sjf-for': function sjfFor() {},
+    'sjf-text': function sjfText(value) {
+      this.innerText = value;
     }
-  }
+  };
 
-  let link = function () {
-    let self = this
+  var link = function link() {
+    var self = this;
     if (!!self._uncompileNodes.length) {
-      self._uncompileNodes.forEach(value => {
-        let attributes = value.attributes
-        console.log(attributes)
-      })
+      self._uncompileNodes.forEach(function (value) {
+        var attributes = value.attributes;
+        console.log(attributes);
+      });
     }
-  }
+  };
 
-  function Sjf (param) {
+  function Sjf(param) {
     if (!param.hasOwnProperty('el') || !param.hasOwnProperty('data')) {
-      console.error('sjf[error]: There is need `data` and `el` attribute')
-      return
+      console.error('sjf[error]: There is need `data` and `el` attribute');
+      return;
     }
-    this._el = document.querySelector(param.el)
-    this._data = param.data
-    this._watchers = []
-    this._uncompileNodes = []
+    this._el = document.querySelector(param.el);
+    this._data = param.data;
+    this._watchers = [];
+    this._uncompileNodes = [];
     for (var method in param.methods) {
-      this['_' + method] = param.methods[method]
+      this['_' + method] = param.methods[method];
     }
-    compile.call(this)
+    compile.call(this);
   }
 
-  return Sjf
-})
+  return Sjf;
+});
