f13c243e8deb126c541ec52f5816471f868bb208
sjffly <1982764150@qq.com>
2017-07-04 17:41:06 +0800

    将指令处理拆分成一个单独的文件

--- source/javascript/render.js
+++ source/javascript/render.js
@@ -1,73 +1,8 @@
 import util from './utils'
-
-const linkRender = {
-  'sjf-if': function (value) {
-    value.node.style.display = (!!(value.expression) ? 'block!important' : 'none!important')
-  },
-  'sjf-show': function (value) {
-    value.node.style.display = (!!(value.expression) ? 'block!important' : 'none!important')
-  },
-  'sjf-for': function (value) {
-    // 将表达式通过空格(不限空格数目)给切开
-    let loopObjectName = value.expression.split(/\s+/)[2]
-    let representativeName = value.expression.split(/\s+/)[0]
-    let toLoopObject = null
-    if (this._data.hasOwnProperty(loopObjectName)) {
-      toLoopObject = this._data[loopObjectName]
-    }
-    // 判断待循环的是否能进行循环
-    let isLoopable = toLoopObject instanceof Array || !isNaN(toLoopObject)
-    if (!isLoopable) {
-      console.error('sjf[error]: the toLoopObject of sjf-for should be a number or an Array')
-      return 
-    }
-    // 判断是数组还是数字，从而赋值length
-    let isArray = util.isArray(toLoopObject)
-    let len = isArray ? toLoopObject.length : toLoopObject
-    let clonedCheckNode = value.node.check.cloneNode(true)
-    let clonedCheckNodeLength = clonedCheckNode.childNodes.length
-    value.node.check.removeAttribute('sjf-for')
-
-    for (let i = 0; i < len; i++) {
-      value.beforeDirectives.map(directive => {
-        if (directive.expression === representativeName) {
-          directive['textNodeValue'] = toLoopObject[i]
-          directive['representativeName'] = representativeName
-          directive['checkNodeChildLength'] = clonedCheckNodeLength
-          linkRender[directive.directive].bind(this)(directive)
-        }
-      })
-      let clonedNode = value.node.check.cloneNode(true)
-      value.node.parent.insertBefore(clonedNode, value.node.check)
-    }
-    value.node.parent.removeChild(value.node.check)
-
-    if (toLoopObject && isArray) {
-      this._watchers.push(toLoopObject)
-    }
-  },
-  'sjf-text': function (value) {
-    console.log(value)
-    let textNodeVlaue = !value.textNodeValue ? this._data[value.expression] : value.textNodeValue
-    let checkNode = value.node.check
-
-    if (value.node.nodeType === 'elementNode') {
-      let textNode = document.createTextNode(textNodeVlaue)
-      let hasChild = checkNode.childNodes.length
-
-      if (checkNode.childNodes.length == value.checkNodeChildLength + 1) {
-        checkNode.removeChild(checkNode.firstChild)
-      }
-      hasChild ? checkNode.insertBefore(textNode, checkNode.firstChild) : checkNode.appendChild(textNode)
-    } else {
-      checkNode.data = textNodeVlaue
-    }
-  }
-}
+import directiveDeal from './directive'
 
 class render {
   constructor (sjf) {
-
     this.sjf = sjf
     this.unBindEvents = []
     this.unSortDirectives = []
@@ -97,7 +32,7 @@ class render {
       }
 
       this.unSortDirectives.map(value => {
-        linkRender[value.directive].bind(this.sjf)(value)
+        directiveDeal[value.directive].bind(this.sjf)(value)
       })
     }
     // this.bindEvent()
@@ -108,17 +43,12 @@ class render {
     let eventQuene = this.unBindEvents
     if (eventQuene.length) {
       eventQuene.map(val => {
-        console.log('val', val)
-        val.target.check.removeAttribute(val.name)
+        let checkNode = val.target.check
         let eventType = util.removePrefix(val.name)
-        console.log(val.func)
         let eventFunc = this.sjf['_' + util.removeBrackets(val.func)]
-        console.log(eventFunc.arguments)
-        if (eventFunc) {
-          val.target.check.addEventListener(eventType, eventFunc, false)
-        } else {
-          console.error('sjf[error]: the ' + val.func + ' is not declared')
-        }
+        checkNode.removeAttribute(val.name)
+
+        eventFunc ? checkNode.addEventListener(eventType, eventFunc, false) : console.error('sjf[error]: the ' + val.func + ' is not declared')
       })
     }
   }
